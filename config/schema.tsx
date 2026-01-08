4bc1a8335432e89c36d6b7ec61c93d86df512077
neev-25 <neevmendapara25@gmail.com>
2026-01-08 20:06:06 +0530

    Landing Screen

--- config/schema.tsx
+++ config/schema.tsx
@@ -1,4 +1,5 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { date, integer, json, pgTable, varchar } from "drizzle-orm/pg-core";
+import { config } from "process";
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
@@ -6,3 +7,13 @@ export const usersTable = pgTable("users", {
   email: varchar({ length: 255 }).notNull().unique(),
   credits:integer().default(5)
 });
+
+export const ProjectTable=pgTable('project',{
+  id:integer().primaryKey().generatedAlwaysAsIdentity(),
+  projectId:varchar().notNull(),
+  useInput:varchar(),
+  device:varchar(),
+  createdOn:date().defaultNow(),
+  config:json(),
+  userId:varchar().references(()=>usersTable.email).notNull()
+})
